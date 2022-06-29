0b7ce2ffff144b29e620b5223ecb07f57c5c31f4
ndreyl <andrelllll@yandex.ru>
2022-06-29 20:15:22 +0300

    Add Function.js

---
+++ Function.js
@@ -0,0 +1,44 @@
+// function yolochka(){
+//     let text_1 = "Hello world!"
+//     console.log(text_1)
+// }
+
+// function yolochka_1(q1, q2){
+
+//     let result = q1 +q2
+//     console.log("Sum ==", q1 * q2)
+// }
+
+// function yolochka_1(q1, q2){
+
+//     if (q1 > 4){
+//         let n_result = q2*10
+//         console.log(n_result)
+//     }
+// }
+
+// yolochka()
+// yolochka_1(5, 20)
+
+// names = ['alex', 'andrey', 'nikita', 'tolya', 'masha']
+
+// function names_editor(person_name){
+//     let name_1 = "Hello " + person_name + "!"
+//     console.log(name_1)
+// }
+
+// for (let i=0; i<names.length; i++){
+//     names_editor(names[i])
+// }
+
+// let f1 = () => console.log("----")
+// f1()
+
+// let t1 = 10;
+// let t2 = 15;
+// let t3 = 20;
+
+// let summ = (n1, n2) => {console.log(`Summ = ${n1 +n2}`)}
+
+// summ(t1,t2)
+
